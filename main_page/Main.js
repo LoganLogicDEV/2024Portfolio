0b2b31aff8120e04dd8ada8a43c5a3715fa1b7de
Logan Hogue <lhogue07@gmail.com>
2024-03-22 10:23:32 -0700

    Removed random sleep function

--- main_page/Main.js
+++ main_page/Main.js
@@ -2,14 +2,6 @@ var i = 0;
 var txt = "I'm Logan Logic";
 var speed = 200;
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
-
 function typeWriter() {
 
     if (i < txt.length) {
